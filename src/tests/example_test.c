44c94d7d77d44332ac460f5cb9eae6c163bbd570
Bryan Franklin <44486392+fra99le@users.noreply.github.com>
2024-02-19 17:50:46 -0500

    One last commit before removing unfinished "example" method.

--- src/tests/example_test.c
+++ src/tests/example_test.c
@@ -26,31 +26,53 @@ double objective_function(double *x, int len) {
 int main() {
     void *fnt = NULL;
 
-    fnt_verbose(2); /* request informative output */
+    fnt_verbose(FNT_INFO); /* request informative output */
     fnt_init(&fnt, FNT_METHODS_DIR "/methods");
 
     /* load example for a DIM parameter problem */
     fnt_set_method(fnt, "example", DIM);
+
+    /* display info */
+    fnt_info(fnt);
+
+    /* set hyper-parameter(s) */
+    int count = 5;
+    fnt_hparam_set(fnt, "count", (void*)&count);
+
+    /* fetch a hyper-parameter */
+    int norm = 0;
+    fnt_hparam_get(fnt, "norm", (void*)&norm);
+    printf("hyper-parameter 'norm' set to %i.\n", norm);
 
     /* print info, if available. */
     if( fnt_info(fnt) != FNT_SUCCESS ) {
         fprintf(stderr, "No info available.\n");
     }
 
-    double *x = calloc(DIM, sizeof(double));
+    /* allocate input for objective function */
+    fnt_vect_t x;
+    fnt_vect_calloc(&x, DIM);
+
+    /* loop as long as method is not complete */
     while( fnt_done(fnt) != FNT_DONE ) {
+
         /* get vector to try */
-        if( fnt_next(fnt, x) != FNT_SUCCESS ) { break; }
+        if( fnt_next(fnt, &x) != FNT_SUCCESS ) { break; }
 
         /* call objective function */
-        double fx = objective_function(x, DIM);
+        double fx = objective_function(x.v, DIM);
 
         /* update method */
-        if( fnt_set_value(fnt, x, fx) != FNT_SUCCESS ) { break; }
+        if( fnt_set_value(fnt, &x, fx) != FNT_SUCCESS ) { break; }
     }
 
-    fnt_result(fnt);
+    /* Get best result. */
+    fnt_minimum(fnt, &x, NULL);
 
+    /* Get/report any results beyond best input vector. */
+    fnt_result(fnt, NULL);
+
+    /* free the method */
     fnt_free(&fnt);
 
     return 0;
